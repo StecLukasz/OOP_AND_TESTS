a4ad50a348fe16f4e39b64a99e9d4977382ecbf1
StecLukasz <lukaszstec14@gmail.com>
2021-01-24 13:58:17 +0100

    NIO odczyt i zapis pliku wielowątkowych

--- src/main/java/edu/lust/sda/io/nio/ReadWriteFileNIO.java
+++ src/main/java/edu/lust/sda/io/nio/ReadWriteFileNIO.java
@@ -1,5 +1,11 @@
 package edu.lust.sda.io.nio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class ReadWriteFileNIO {
     /**
      * NIO używa wielowątkowości
@@ -8,4 +14,31 @@ public class ReadWriteFileNIO {
      *
      *
      * */
+
+    public static void main(String[] args) {
+        ReadWriteFileNIO readWriteFileNIO = new ReadWriteFileNIO();
+        String filename = "NIOFile.txt";
+
+        try {
+            readWriteFileNIO.simpleWriteToFile(filename);
+            readWriteFileNIO.simpleReadFromFile(filename);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd");
+        }
+    }
+    public void simpleWriteToFile(String filename) throws IOException {
+        System.out.println("Początek zapisu do pliku");
+        Files.writeString(Paths.get(filename),"FirstLine");
+        Files.writeString(Paths.get(filename),"\nSecondLine", StandardOpenOption.APPEND);
+        Files.writeString(Paths.get(filename),"\nThirdLine",StandardOpenOption.APPEND);
+        System.out.println("Koniec zapisu do pliku");
+    }
+
+    public void simpleReadFromFile(String filename) throws IOException{
+        List<String> filecontent;
+        System.out.println("Odczyt z pliku");
+        filecontent = Files.readAllLines(Paths.get(filename));
+        filecontent.forEach(System.out::println);
+        System.out.println("Koniec odczytu z pliku");
+    }
 }
